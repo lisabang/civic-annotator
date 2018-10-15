2567085d741932db3ecfc96fa8e5d919deae31e8
lisabang@github.com <gbang@geisinger.edu>
2018-10-15 16:38:58 -0700

    it is going to work

--- civic-annotator.py
+++ civic-annotator.py
@@ -16,10 +16,34 @@ class CravatAnnotator(BaseAnnotator):
         sqlite3.Connection object is stored as self.dbconn, and the 
         sqlite3.Cursor object is stored as self.cursor.
         """
-        print("dlwfno")
+        #print("dlwfno")
         #self.lifter = LiftOver(constants.liftover_chain_paths[self.input_assembly])
-        pass
-    
+        r = requests.get('https://civicdb.org/api/variants?count=5000&page=1')
+        variants=json.loads(r.text)['records']
+        #variants = civic.get_variants_by_ids(civic.get_all_variant_ids())
+        lifter = LiftOver(constants.liftover_chain_paths['hg19'])
+        print("lifted variants")
+        vdict = {}
+        for variant in variants:
+            chrom_37 = variant['coordinates']['chromosome']
+            pos_37 = variant['coordinates']['start']
+            if chrom_37 is None or pos_37 is None: continue
+            new_coords = lifter.convert_coordinate("chr" + chrom_37, int(pos_37))
+            if len(new_coords) > 0:
+                chrom_38 = new_coords[0][0].replace('chr','')
+                pos_38 = new_coords[0][1]
+            else:
+                continue
+            ref = variant['coordinates']['reference_bases']
+            alt = variant['coordinates']['variant_bases']
+            toks = [chrom_38, pos_38, ref, alt]
+            if None not in toks:
+                vkey = ':'.join(map(str, toks))
+                vdict[vkey] = variant
+            else:
+                continue
+        self.civicdata = vdict
+
     def annotate(self, input_data, secondary_data=None):
         """
         The annotator parent class will call annotate for each line of the 
@@ -53,8 +77,8 @@ class CravatAnnotator(BaseAnnotator):
         out['placeholder_annotation'] = ":".join([input_data["chrom"],str(input_data["pos"]),input_data["ref_base"],input_data["alt_base"]])
         match=self.civicdata.get(out["placeholder_annotation"], False)
         if match:
-            out["description"]=match.description
-            out["clinical_a_score"]=match.civic_actionability_score
+            out["description"]=match['description']
+            out["clinical_a_score"]=match['civic_actionability_score']
         return out
     
     def cleanup(self):
--- test-setup.py
+++ test-setup.py
@@ -1,21 +1,26 @@
 from civicpy import civic
 from pyliftover import LiftOver
 from cravat import constants
-
-variants = civic.get_variants_by_ids(civic.get_all_variant_ids())
+import requests
+import json
+r = requests.get('https://civicdb.org/api/variants?count=5000&page=1')
+variants=json.loads(r.text)['records']
+#variants = civic.get_variants_by_ids(civic.get_all_variant_ids())
 lifter = LiftOver(constants.liftover_chain_paths['hg19'])
+print("lifted variants")
 vdict = {}
 for variant in variants:
-    chrom_37 = 'chr'+variant.coordinates.chromosome
-    pos_37 = variant.coordinates.start
-    new_coords = lifter.convert_coordinate(chrom_37, int(pos_37))
+    chrom_37 = variant['coordinates']['chromosome']
+    pos_37 = variant['coordinates']['start']
+    if chrom_37 is None or pos_37 is None: continue
+    new_coords = lifter.convert_coordinate("chr" + chrom_37, int(pos_37))
     if len(new_coords) > 0:
         chrom_38 = new_coords[0][0].replace('chr','')
         pos_38 = new_coords[0][1]
     else:
         continue
-    ref = variant.coordinates.reference_bases
-    alt = variant.coordinates.variant_bases
+    ref = variant['coordinates']['reference_bases']
+    alt = variant['coordinates']['variant_bases']
     toks = [chrom_38, pos_38, ref, alt]
     if None not in toks:
         vkey = ':'.join(map(str, toks))
